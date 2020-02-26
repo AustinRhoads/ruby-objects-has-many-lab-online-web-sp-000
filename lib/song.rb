658609093aadccdc3afc44feeb85d55867c05c78
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-26 02:34:05 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+attr_accessor :name, :artist, :genre
+
+@@all = []
+
+def initialize (name, genre = nil)
+@name = name
+@genre = genre
+save
+end
+
+def save
+@@all << self
+end
+
+def artist_name
+self.artist.name
+end
+
+def self.all
+  @@all
+end
+
+end
