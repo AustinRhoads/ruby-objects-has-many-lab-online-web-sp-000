9decf5dfeedc3559aa95552836524e796757a8ec
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-26 02:46:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,10 @@
 class Artist
 attr_accessor :name
-
+@@all = []
 @@all_artist_songs = []
 def initialize (name)
 @name = name
-
+@@all << self
 end
 
 def add_song(song)
@@ -23,7 +23,8 @@ def songs
 end
 
 def self.song_count
-
+@@all.each {|artist| @@all_artist_songs << artist.songs }
+@@all_artist_songs.length
 end
 
 
